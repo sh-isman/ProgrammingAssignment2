ea5b3a1b1070b33b94288ec95e34c0c410c8efd5
sh-isman <shakhrukh.ismonov@gmail.com>
2015-02-12 21:40:24 -0500

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Created 2/12/2015
+## makeCacheMatrix and cacheSolve functions work together to
+## speed up the code where inverse of the matrix is computed for the same matrix
+## so many times. 
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a list that keeps record of inverse of the matrix, if it was computed before
+## if the inverse is not computed yet, then it will have a flag (m=NULL), indicating that it is a virgin matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## the function cacheSolve solve for the inverse of the matrix if the matrix is virgin (ie: inverse not computed before)
+## otherwise, it simply returns, previously computed inverse saved by makeChacheMatrixs
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setinv(m)
+  m
 }
